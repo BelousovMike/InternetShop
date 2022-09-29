80ad7c9a21586618f3c90b240c8d297ac839f68d
Михаил Белоусов <m.belousov@innopolis.ru>
2022-09-29 13:52:24 +0300

    Add project files.

---
+++ DataAccessLayer/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Model { get; set; }
+
+        public string Code { get; set; }
+
+        public int CompanyId { get; set; }
+
+
+    }
+}
